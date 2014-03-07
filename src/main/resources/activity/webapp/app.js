12f00f16d52209900eea28f477e98cb36aa357fb
Matt Vollrath <matt@endpoint.com>
2014-03-07 11:38:06 -0500

    Hook up a couple of Earth queries in webctl
    Eiffel Tower and Grand Canyon.

--- src/main/resources/activity/webapp/app.js
+++ src/main/resources/activity/webapp/app.js
@@ -192,16 +192,53 @@ LiquidGalaxyApp
   /**
    * Points of Interest for each planet.
    */
+  /* 
+   * <LookAt>
+   * <longitude>2.292014985093174</longitude>
+   * <latitude>48.86067281595832</latitude>
+   * <altitude>0</altitude>
+   * <heading>-30.82811991881539</heading>
+   * <tilt>68.12334319265283</tilt>
+   * <range>870.2504450963008</range>
+   * <altitudeMode>relativeToGround</altitudeMode>
+   * </LookAt>
+   */
   .value('PoiContent', {
     'earth': [
       { style: "outdoor", title: "Trevi Fountain, Rome", type: "earth" },
-      { style: "outdoor", title: "Eiffel Tower, Paris", type: "earth" },
+      { style: "outdoor", title: "Eiffel Tower, Paris", type: "earth", abstractView: {
+        type: "lookat",
+        location: {
+          altitude: 0,
+          latitude: 48.86067281595832,
+          longitude: 2.292014985093174
+        },
+        orientation: {
+          heading: -30.82811991881539,
+          range: 870.2504450963008,
+          tilt: 68.12334319265283
+        },
+        altitudeMode: "relativeToGround"
+      } },
       { style: "street", title: "Sunset Boulevard, Los Angeles", type: "streetview", panoid: "o4yrNps7LEEGNTssM-xiDg", heading: 50 },
       { style: "ocean", title: "Heron Island, Great Barrier Reef", type: "streetview", panoid: "CWskcsTEZBNXaD8gG-zATA", heading: -25 },
       { style: "outdoor", title: "Guylian Chocolate Store, Sydney", type: "earth" },
       { style: "outdoor", title: "Saint John Cathedral, New York", type: "earth" },
       { style: "indoor", title: "Opera House, Sydney", type: "streetview", panoid: "j-ItqSf62H4AAAAGOzBYXA", heading: 0 },
-      { style: "outdoor", title: "The Grand Canyon", type: "earth" },
+      { style: "outdoor", title: "The Grand Canyon", type: "earth", abstractView: {
+        type: "lookat",
+        location: {
+          altitude: 0,
+          latitude: 36.03873353926406,
+          longitude: -111.8889022015411
+        },
+        orientation: {
+          heading: -85.87896607649738,
+          range: 5355.631620407577,
+          tilt: 67.92562079561495
+        },
+        altitudeMode: "relativeToGround"
+      } },
       { style: "indoor", title: "State Dining Room,<br />The White House", type: "streetview", panoid: "I5NDPRik49udZwxm4LYdCQ", heading: 90 },
       { style: "outdoor", title: "GBus Locations<br />(updated @ 10 mins)", type: "earth" }
     ],
