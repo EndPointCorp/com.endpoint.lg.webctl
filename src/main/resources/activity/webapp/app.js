e2dec08f9a24edde8bab18111660c8b7242bd127
Matt Vollrath <matt@endpoint.com>
2014-03-08 18:04:14 -0500

    Consistent module definition style for webctl app

--- src/main/resources/activity/webapp/app.js
+++ src/main/resources/activity/webapp/app.js
@@ -1,221 +1,219 @@
 /**
  * A somewhat monolithic Angular module for this webapp.
  */
-var LiquidGalaxyApp = angular.module('LiquidGalaxyApp', ['ngSanitize', 'IS.MasterModule', 'IS.MessageModule']);
+var LiquidGalaxyApp = angular.module('LiquidGalaxyApp', ['ngSanitize', 'IS.MasterModule', 'IS.MessageModule'])
 
-LiquidGalaxyApp
+/**
+ * Apps are user-facing applications that fill the main displays. 
+ */
+.value('Apps', {
+  StreetView: 'streetview',
+  Earth: 'earth'
+})
 
-  /**
-   * Apps are user-facing applications that fill the main displays. 
-   */
-  .value('Apps', {
-    StreetView: 'streetview',
-    Earth: 'earth'
-  })
+/**
+ * ActivityGroups are the names of activity groups for Apps.
+ */
+.value('ActivityGroups', {
+  StreetView: IS.Configuration['lg.webctl.group.streetview'],
+  Earth: IS.Configuration['lg.webctl.group.earth']
+})
 
-  /**
-   * ActivityGroups are the names of activity groups for Apps.
-   */
-  .value('ActivityGroups', {
-    StreetView: IS.Configuration['lg.webctl.group.streetview'],
-    Earth: IS.Configuration['lg.webctl.group.earth']
-  })
+/**
+ * Modes are states of map operation, for toggling Street View coverage layer and exiting Street View.
+ */
+.value('Modes', {
+  StreetView: 'streetview',
+  Earth: 'earth'
+})
 
-  /**
-   * Modes are states of map operation, for toggling Street View coverage layer and exiting Street View.
-   */
-  .value('Modes', {
-    StreetView: 'streetview',
-    Earth: 'earth'
-  })
+/**
+ * Planets are solar bodies.
+ */
+.value('Planets', {
+  Earth: 'earth',
+  Moon: 'moon',
+  Mars: 'mars'
+})
 
-  /**
-   * Planets are solar bodies.
-   */
-  .value('Planets', {
-    Earth: 'earth',
-    Moon: 'moon',
-    Mars: 'mars'
-  })
+/**
+ * Extra configuration for map behaviors.
+ */
+.value('MapConfig', {
+  MapZoomMax: 18,
+  MapZoomMin: 1,
+  MinStreetViewZoomLevel: 14,
+  MapClickSearchRadius: 50,
+  MapZoomFudge: -1,
+  EarthAltitudeMin: 100,
+  EarthAltitudeMax: 100000000,
+  DefaultCenter: new google.maps.LatLng(-34.397, 150.644)
+})
 
-  /**
-   * Extra configuration for map behaviors.
-   */
-  .value('MapConfig', {
-    MapZoomMax: 18,
-    MapZoomMin: 1,
-    MinStreetViewZoomLevel: 14,
-    MapClickSearchRadius: 50,
-    MapZoomFudge: -1,
-    EarthAltitudeMin: 100,
-    EarthAltitudeMax: 100000000,
-    DefaultCenter: new google.maps.LatLng(-34.397, 150.644)
-  })
+/**
+ * Messages types for websocket communication.
+ */
+.value('Messages', {
+  Earth: {
+    Activate: 'Earth.activate',
+    Deactivate: 'Earth.deactivate',
+    SetView: 'Earth.setView',
+    SetPlanet: 'Earth.setPlanet',
+    Search: 'Earth.search',
+    ViewChanged: 'Earth.viewChanged'
+  },
+  StreetView: {
+    Activate: 'StreetView.activate',
+    Deactivate: 'StreetView.deactivate',
+    SetPano: 'StreetView.setPano',
+    SetPov: 'StreetView.setPov',
+    PanoChanged: 'StreetView.panoChanged',
+    PovChanged: 'StreetView.povChanged'
+  }
+})
 
-  /**
-   * Messages types for websocket communication.
-   */
-  .value('Messages', {
-    Earth: {
-      Activate: 'Earth.activate',
-      Deactivate: 'Earth.deactivate',
-      SetView: 'Earth.setView',
-      SetPlanet: 'Earth.setPlanet',
-      Search: 'Earth.search',
-      ViewChanged: 'Earth.viewChanged'
-    },
-    StreetView: {
-      Activate: 'StreetView.activate',
-      Deactivate: 'StreetView.deactivate',
-      SetPano: 'StreetView.setPano',
-      SetPov: 'StreetView.setPov',
-      PanoChanged: 'StreetView.panoChanged',
-      PovChanged: 'StreetView.povChanged'
-    }
-  })
+/**
+ * Event types for controller communication.
+ */
+.value('UIEvents', {
+  Keyboard: {
+    KeyPress: 'Keyboard.keyPress'
+  },
+  Map: {
+    ZoomChanged: 'Map.zoomChanged',
+    SelectPano: 'Map.selectPano'
+  },
+  Mode: {
+    SelectMode: 'Mode.selectMode'
+  },
+  Planet: {
+    SelectPlanet: 'Planet.selectPlanet'
+  },
+  Poi: {
+    SelectPoi: 'Poi.selectPoi'
+  },
+  Search: {
+    Query: 'Search.query',
+    Activated: 'Search.activated',
+    Deactivated: 'Search.deactivated'
+  }
+})
 
-  /**
-   * Event types for controller communication.
-   */
-  .value('UIEvents', {
-    Keyboard: {
-      KeyPress: 'Keyboard.keyPress'
-    },
-    Map: {
-      ZoomChanged: 'Map.zoomChanged',
-      SelectPano: 'Map.selectPano'
-    },
-    Mode: {
-      SelectMode: 'Mode.selectMode'
-    },
-    Planet: {
-      SelectPlanet: 'Planet.selectPlanet'
-    },
-    Poi: {
-      SelectPoi: 'Poi.selectPoi'
-    },
-    Search: {
-      Query: 'Search.query',
-      Activated: 'Search.activated',
-      Deactivated: 'Search.deactivated'
-    }
-  })
-
-  /**
-   * Style selections for the map.
-   */
-  .value('MapStyles', [
-    {
-      featureType: "poi",
-      elementType: "all",
-      stylers: [
-        { visibility: "off" }
-      ]
-    },
-    {
-      featureType: "administrative",
-      elementType: "all",
-      stylers: [
-        { visibility: "off" }
-      ]
-    },
-    {
-      featureType: "administrative.province",
-      elementType: "all",
-      stylers: [
-        { visibility: "on" }
-      ]
-    },
-    {
-      featureType: "administrative.country",
-      elementType: "all",
-      stylers: [
-        { visibility: "on" }
-      ]
-    },
-    {
-      featureType: "transit",
-      elementType: "all",
-      stylers: [
-        { visibility: "off" }
-      ]
-    },
-  ])
-
-  .value('QueryMessageFields', {
-    Planet: {
-      Destination: 'destination'
-    },
-    Search: {
-      Query: 'query'
-    }
-  })
-
-  .value('FlyToMessageFields', {
-    Type: 'type',
-    Location: 'location',
-    Altitude: 'altitude',
-    Orientation: 'orientation'
-  })
-
-  /**
-   * Points of Interest for each planet.
-   */
-  .value('PoiContent', {
-    'earth': [
-      { style: "outdoor", title: "Trevi Fountain, Rome", type: "earth" },
-      { style: "outdoor", title: "Eiffel Tower, Paris", type: "earth", abstractView: {
-        type: "lookat",
-        location: {
-          altitude: 0,
-          latitude: 48.86067281595832,
-          longitude: 2.292014985093174
-        },
-        orientation: {
-          heading: -30.82811991881539,
-          range: 870.2504450963008,
-          tilt: 68.12334319265283
-        },
-        altitudeMode: "relativeToGround"
-      } },
-      { style: "street", title: "Sunset Boulevard, Los Angeles", type: "streetview", panoid: "o4yrNps7LEEGNTssM-xiDg", heading: 50 },
-      { style: "ocean", title: "Heron Island, Great Barrier Reef", type: "streetview", panoid: "CWskcsTEZBNXaD8gG-zATA", heading: -25 },
-      { style: "outdoor", title: "Guylian Chocolate Store, Sydney", type: "earth" },
-      { style: "outdoor", title: "Saint John Cathedral, New York", type: "earth" },
-      { style: "indoor", title: "Opera House, Sydney", type: "streetview", panoid: "j-ItqSf62H4AAAAGOzBYXA", heading: 0 },
-      { style: "outdoor", title: "The Grand Canyon", type: "earth", abstractView: {
-        type: "lookat",
-        location: {
-          altitude: 0,
-          latitude: 36.03873353926406,
-          longitude: -111.8889022015411
-        },
-        orientation: {
-          heading: -85.87896607649738,
-          range: 5355.631620407577,
-          tilt: 67.92562079561495
-        },
-        altitudeMode: "relativeToGround"
-      } },
-      { style: "indoor", title: "State Dining Room,<br />The White House", type: "streetview", panoid: "I5NDPRik49udZwxm4LYdCQ", heading: 90 },
-      { style: "outdoor", title: "GBus Locations<br />(updated @ 10 mins)", type: "earth" }
-    ],
-    'moon': [
-      { style: "outdoor", title: "Apollo 11", type: "earth" },
-      { style: "outdoor", title: "Mare Ingenii", type: "earth" },
-      { style: "outdoor", title: "Lacus Excellentiae", type: "earth" },
-      { style: "outdoor", title: "Sinus Concordiae", type: "earth" },
-      { style: "outdoor", title: "Mons Huygens", type: "earth" },
-      { style: "outdoor", title: "Montes Riphaeus", type: "earth" },
-      { style: "outdoor", title: "Rima Cleopatra", type: "earth" }
-    ],
-    'mars': [
-      { style: "outdoor", title: "Curiosity Landing Site", type: "earth" },
-      { style: "outdoor", title: "Opportunity Landing Site", type: "earth" },
-      { style: "outdoor", title: "Olympus Mons", type: "earth" },
-      { style: "outdoor", title: "Amazonis Planitia", type: "earth" },
-      { style: "outdoor", title: "Solis Planum", type: "earth" },
-      { style: "outdoor", title: "Chasma Boreale", type: "earth" },
-      { style: "outdoor", title: "Valles Marineris", type: "earth" }
+/**
+ * Style selections for the map.
+ */
+.value('MapStyles', [
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+      { visibility: "off" }
     ]
-  });
+  },
+  {
+    featureType: "administrative",
+    elementType: "all",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },
+  {
+    featureType: "administrative.province",
+    elementType: "all",
+    stylers: [
+      { visibility: "on" }
+    ]
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "all",
+    stylers: [
+      { visibility: "on" }
+    ]
+  },
+  {
+    featureType: "transit",
+    elementType: "all",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },
+])
+
+.value('QueryMessageFields', {
+  Planet: {
+    Destination: 'destination'
+  },
+  Search: {
+    Query: 'query'
+  }
+})
+
+.value('FlyToMessageFields', {
+  Type: 'type',
+  Location: 'location',
+  Altitude: 'altitude',
+  Orientation: 'orientation'
+})
+
+/**
+ * Points of Interest for each planet.
+ */
+.value('PoiContent', {
+  'earth': [
+    { style: "outdoor", title: "Trevi Fountain, Rome", type: "earth" },
+    { style: "outdoor", title: "Eiffel Tower, Paris", type: "earth", abstractView: {
+      type: "lookat",
+      location: {
+        altitude: 0,
+        latitude: 48.86067281595832,
+        longitude: 2.292014985093174
+      },
+      orientation: {
+        heading: -30.82811991881539,
+        range: 870.2504450963008,
+        tilt: 68.12334319265283
+      },
+      altitudeMode: "relativeToGround"
+    } },
+    { style: "street", title: "Sunset Boulevard, Los Angeles", type: "streetview", panoid: "o4yrNps7LEEGNTssM-xiDg", heading: 50 },
+    { style: "ocean", title: "Heron Island, Great Barrier Reef", type: "streetview", panoid: "CWskcsTEZBNXaD8gG-zATA", heading: -25 },
+    { style: "outdoor", title: "Guylian Chocolate Store, Sydney", type: "earth" },
+    { style: "outdoor", title: "Saint John Cathedral, New York", type: "earth" },
+    { style: "indoor", title: "Opera House, Sydney", type: "streetview", panoid: "j-ItqSf62H4AAAAGOzBYXA", heading: 0 },
+    { style: "outdoor", title: "The Grand Canyon", type: "earth", abstractView: {
+      type: "lookat",
+      location: {
+        altitude: 0,
+        latitude: 36.03873353926406,
+        longitude: -111.8889022015411
+      },
+      orientation: {
+        heading: -85.87896607649738,
+        range: 5355.631620407577,
+        tilt: 67.92562079561495
+      },
+      altitudeMode: "relativeToGround"
+    } },
+    { style: "indoor", title: "State Dining Room,<br />The White House", type: "streetview", panoid: "I5NDPRik49udZwxm4LYdCQ", heading: 90 },
+    { style: "outdoor", title: "GBus Locations<br />(updated @ 10 mins)", type: "earth" }
+  ],
+  'moon': [
+    { style: "outdoor", title: "Apollo 11", type: "earth" },
+    { style: "outdoor", title: "Mare Ingenii", type: "earth" },
+    { style: "outdoor", title: "Lacus Excellentiae", type: "earth" },
+    { style: "outdoor", title: "Sinus Concordiae", type: "earth" },
+    { style: "outdoor", title: "Mons Huygens", type: "earth" },
+    { style: "outdoor", title: "Montes Riphaeus", type: "earth" },
+    { style: "outdoor", title: "Rima Cleopatra", type: "earth" }
+  ],
+  'mars': [
+    { style: "outdoor", title: "Curiosity Landing Site", type: "earth" },
+    { style: "outdoor", title: "Opportunity Landing Site", type: "earth" },
+    { style: "outdoor", title: "Olympus Mons", type: "earth" },
+    { style: "outdoor", title: "Amazonis Planitia", type: "earth" },
+    { style: "outdoor", title: "Solis Planum", type: "earth" },
+    { style: "outdoor", title: "Chasma Boreale", type: "earth" },
+    { style: "outdoor", title: "Valles Marineris", type: "earth" }
+  ]
+});
